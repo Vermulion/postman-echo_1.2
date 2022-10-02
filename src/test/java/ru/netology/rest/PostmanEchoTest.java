3a9b50f24adcfbea95ebf43db65db5d814d27cb6
Viacheslav Ryabov <vermulion@gmail.com>
2022-10-02 23:50:15 +0300

    Postman Echo crash test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -39,6 +39,6 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Пути творчества №5"));
+                .body("data", equalTo("any data"));
     }
 }

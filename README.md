a2061a716e8a479c62b382ac742ca81f43916907
Viacheslav Ryabov <vermulion@gmail.com>
2022-10-02 23:44:32 +0300

    Postman Echo test project created

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Project Postman_Echo_1.2
-[![Build status](https://ci.appveyor.com/api/projects/status/ritw2mvnh1xkc380?svg=true)](https://ci.appveyor.com/project/Vermulion/api-ci-1-2)
--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -4,22 +4,41 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class PostmanEchoTest {
     @Test
+            void shouldReturnPostQuery() {
 // Given - When - Then
 // Предусловия
-    given()
-  .baseUri("https://postman-echo.com")
-  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-.when()
-  .post("/post")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/post")
 // Проверки
-.then()
-  .statusCode(200)
-  .body(/* --> ваша проверка здесь <-- */);
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+    @Test
+    void shouldReturnPostQueryRussian() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Пути творчества №5") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Пути творчества №5"));
     }
 }
